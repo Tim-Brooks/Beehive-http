28a7e49e324eb1c94b4bcf29c7d119fb606b4fb5
Tim Brooks <tim@uncontended.net>
2015-12-20 22:09:12 -0600

    Update to new version of beehive

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.uncontended/Precipice-Implementations "0.1.0"]
-                 [net.uncontended/beehive "0.5.3"]])
+                 [net.uncontended/beehive "0.5.4"]])
