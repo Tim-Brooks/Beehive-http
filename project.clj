cdea6b88f6f643a6f426e35d5b2903b1604c05ab
Tim Brooks <tim@uncontended.net>
2015-12-21 19:47:53 -0600

    Remove incorrect license in project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject beehive-http "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.ning/async-http-client "1.9.31"]
